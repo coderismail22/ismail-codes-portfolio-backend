af6b3f09adf40f87cbcf13a0ad78f4edbfa9030a
ismail <coderismail22@gmail.com>
2024-10-27 13:36:42 +0600

    update email address

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -5,12 +5,13 @@ import config from "../../config";
 const sendEmailToIsmail = async (emailData: TEmail) => {
   const { name, country, email, message } = emailData;
   const transporter = nodemailer.createTransport({
+    // TODO: change email to ismailmdhossain2@gmail.com
     host: "smtp.gmail.com",
     port: 587,
     secure: config.node_env === "production", // true for port 465, false for other ports
     auth: {
-      user: "xordiboy@gmail.com",
-      pass: "aesc tcrj ushj gjlc",
+      user: "ismailmdhossain2@gmail.com",
+      pass: "uume cyrd vtdn orwq",
     },
     tls: {
       rejectUnauthorized: false,
@@ -18,9 +19,9 @@ const sendEmailToIsmail = async (emailData: TEmail) => {
   });
 
   const mailOptions = {
-    from: `xordiboy@gmail.com`, // sender address
-    to: "xordiboy@gmail.com", // recipient's email
-    subject: "Connection Message",
+    from: `ismailmdhossain2@gmail.com`, // sender address
+    to: "ismailmdhossain2@gmail.com", // recipient's email
+    subject: "Connection Request From Portfolio",
     text: `Name: ${name}\nCountry: ${country}\nEmail: ${email}\nMessage: ${message}`,
     html: `<p>Name: ${name}</p><p>Country: ${country}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
   };
